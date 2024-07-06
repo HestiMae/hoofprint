7fda68107cf43e7711ec9ec7176f160d02ab7777
HestiMae <hestiaheartstrings@gmail.com>
2024-07-06 15:32:13 +1000

    block accurate centred map

--- src/main/java/garden/hestia/hoofprint/HoofprintScreen.java
+++ src/main/java/garden/hestia/hoofprint/HoofprintScreen.java
@@ -9,7 +9,10 @@ import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.text.Text;
+import net.minecraft.util.math.BlockPointer;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.Biome;
 
@@ -23,17 +26,22 @@ public class HoofprintScreen extends Screen {
 
     @Override
     public void render(DrawContext context, int mouseX, int mouseY, float delta) {
-        ChunkPos originChunk = MinecraftClient.getInstance().player.getChunkPos();
+        Vec3d origin = MinecraftClient.getInstance().player.getPos();
+        int playerX = (int) Math.floor(origin.x);
+        int playerZ = (int) Math.floor(origin.z);
+        ChunkPos originChunk = new ChunkPos(playerX / 16, playerZ / 16);
+        int chunkRelativeX = playerX - (originChunk.x * 16);
+        int chunkRelativeZ = playerZ - (originChunk.z * 16);
         if (this.mapStorage != null) {
             try (FillBatcher batcher = new FillBatcher(context))
             {
-                int xSize = (int) Math.ceil((float) this.width / 16);
-                int zSize = (int) Math.ceil((float) this.height / 16);
-                for (int chunkX = 0; chunkX < xSize; chunkX++)
+                int xSize = (int) Math.ceil((float) this.width / 16) + 4;
+                int zSize = (int) Math.ceil((float) this.height / 16) + 4;
+                for (int chunkX = -2; chunkX < xSize; chunkX++)
                 {
-                    for (int chunkZ = 0; chunkZ < zSize; chunkZ++)
+                    for (int chunkZ = -2; chunkZ < zSize; chunkZ++)
                     {
-                        ChunkPos pos = new ChunkPos(chunkX + originChunk.x - (xSize / 2), chunkZ + originChunk.z - (zSize / 2));
+                        ChunkPos pos = new ChunkPos(chunkX + originChunk.x - Math.round(xSize / 2.0f) + 2, chunkZ + originChunk.z - Math.round(zSize / 2.0f) + 2);
                         LayerSummary.Raw layer = mapStorage.bakedTerrain.get(pos);
                         RegistryPalette<Biome>.ValueView biomePalette = mapStorage.biomePalettes.get(pos);
                         RegistryPalette<Block>.ValueView blockPalette = mapStorage.blockPalettes.get(pos);
@@ -42,14 +50,17 @@ public class HoofprintScreen extends Screen {
                             int[][] colors = this.getColors(layer, biomePalette, blockPalette);
                             for (int x = 0; x < colors.length; x++) {
                                 for (int z = 0; z < colors[x].length; z++) {
-                                    batcher.add(chunkX * 16 + x, chunkZ * 16 + z, chunkX * 16 + x+1, chunkZ * 16 + z+1, 0, colors[x][z]);
+                                    int x1 = chunkX * 16 + x - chunkRelativeX;
+                                    int y1 = chunkZ * 16 + z - chunkRelativeZ;
+
+                                    if (x1 <= width && y1 <= height) batcher.add(x1, y1, x1 + 1, y1+1, 0, colors[x][z]);
                                 }
                             }
                         }
                     }
                 }
             }
-
+            context.fill(width / 2, height / 2 , (width / 2) + 1, (height / 2) + 1, 0xff39ff14);
         }
         super.render(context, mouseX, mouseY, delta);
     }
@@ -69,5 +80,10 @@ public class HoofprintScreen extends Screen {
         RegistryKey<World> dim = MinecraftClient.getInstance().world.getRegistryKey();
         this.mapStorage = HoofprintMapStorage.get(dim);
         super.init();
+    }
+
+    @Override
+    public boolean shouldPause() {
+        return false;
     }
 }
