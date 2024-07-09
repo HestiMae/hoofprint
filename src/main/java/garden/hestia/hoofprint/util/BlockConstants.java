2e2250d1c087a13c736fde00095baa27f71b304f
HestiMae <hestiaheartstrings@gmail.com>
2024-07-09 14:53:34 +1000

    change GRASS_BLOCKS to GRASS

--- src/main/java/garden/hestia/hoofprint/util/BlockConstants.java
+++ src/main/java/garden/hestia/hoofprint/util/BlockConstants.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class BlockConstants {
     public static final List<Block> FOLIAGE_BLOCKS = List.of(Blocks.OAK_LEAVES, Blocks.JUNGLE_LEAVES,
         Blocks.ACACIA_LEAVES, Blocks.DARK_OAK_LEAVES, Blocks.VINE);
-    public static final List<Block> GRASS_BLOCKS = List.of(Blocks.SHORT_GRASS, Blocks.TALL_GRASS,
+    public static final List<Block> GRASS_BLOCKS = List.of(Blocks.GRASS, Blocks.TALL_GRASS,
         Blocks.FERN, Blocks.POTTED_FERN, Blocks.LARGE_FERN, Blocks.SUGAR_CANE);
     public static final List<Block> GRASS_BLOCK_BLOCKS = List.of(Blocks.GRASS_BLOCK);
     public static final List<Block> STONE_BLOCKS = List.of(Blocks.STONE, Blocks.ANDESITE);
