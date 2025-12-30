94e1558d22e95c4bd478bd2346d929fc993b6643
sisby-folk <sleepingdragoninn@gmail.com>
2025-12-30 12:37:09 +1100

    use scale for order

--- src/main/java/garden/hestia/hoofprint/HoofprintConfig.java
+++ src/main/java/garden/hestia/hoofprint/HoofprintConfig.java
@@ -3,7 +3,6 @@ package garden.hestia.hoofprint;
 import folk.sisby.kaleido.api.WrappedConfig;
 import folk.sisby.kaleido.lib.quiltconfig.api.annotations.Comment;
 import folk.sisby.kaleido.lib.quiltconfig.api.annotations.IntegerRange;
-import folk.sisby.kaleido.lib.quiltconfig.api.values.ValueList;
 import folk.sisby.kaleido.lib.quiltconfig.api.values.ValueMap;
 import folk.sisby.surveyor.client.SurveyorClient;
 import garden.hestia.hoofprint.util.ConstantLightMap;
@@ -75,25 +74,21 @@ public class HoofprintConfig extends WrappedConfig {
 	public Dimensions dimensions = new Dimensions();
 
 	public static class Dimensions implements Section {
-		@Comment("Cycle order of dimension maps.")
-		@Comment("Automatically populates after loading into the world.")
-		public List<String> order = ValueList.create("", "minecraft:overworld", "minecraft:the_nether", "minecraft:the_end");
-
-		public List<RegistryKey<World>> getOrder(ClientPlayNetworkHandler handler) {
-			List<RegistryKey<World>> dims = new ArrayList<>(SurveyorClient.getSummaries(handler).keySet().stream().sorted(Comparator.comparing(RegistryKey::toString)).toList());
-			dims.removeIf(dim -> HoofprintMapStorage.get(dim).isEmpty());
-			order.removeIf(v -> handler.getWorldKeys().stream().noneMatch(d -> d.getValue().toString().equals(v)));
-			dims.stream().filter(dim -> !order.contains(dim.getValue().toString())).forEach(dim -> order.add(dim.getValue().toString()));
-			return dims.stream().sorted(Comparator.comparing(dim -> order.indexOf(dim.getValue().toString()))).toList();
-		}
-
-		@Comment("Coordinate scales of each dimension.")
+		@Comment("Cycle order and coordinate scales of each dimension.")
 		@Comment("If not 0, the relative position of the player will be shown.")
 		public Map<String, Integer> scales = ValueMap.builder(0)
 			.put("minecraft:overworld", 8)
 			.put("minecraft:the_nether", 1)
 			.put("minecraft:the_end", 0)
 			.build();
+
+		public List<RegistryKey<World>> getOrder(ClientPlayNetworkHandler handler) {
+			List<RegistryKey<World>> dims = new ArrayList<>(SurveyorClient.getSummaries(handler).keySet().stream().sorted(Comparator.comparing(RegistryKey::toString)).toList());
+			dims.removeIf(dim -> HoofprintMapStorage.get(dim).isEmpty());
+			scales.keySet().removeIf(v -> handler.getWorldKeys().stream().noneMatch(d -> d.getValue().toString().equals(v)));
+			dims.stream().filter(dim -> !scales.containsKey(dim.getValue().toString())).forEach(dim -> scales.put(dim.getValue().toString(), 0));
+			return dims.stream().sorted(Comparator.comparing(dim -> scales.keySet().stream().toList().indexOf(dim.getValue().toString()))).toList();
+		}
 
 		public Map<RegistryKey<World>, Integer> getScales(ClientPlayNetworkHandler handler) {
 			List<RegistryKey<World>> dims = new ArrayList<>(SurveyorClient.getSummaries(handler).keySet().stream().sorted(Comparator.comparing(RegistryKey::toString)).toList());
